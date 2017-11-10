1811a29f965825ace087403445a1575e65b427ca
curoles <curoles@yahoo.com>
2017-11-10 02:11:47 -0800

    spdoc: make next todo list

--- spdoc/tools/spdoc-ticket.rb
+++ spdoc/tools/spdoc-ticket.rb
@@ -2,16 +2,40 @@
 # http://www.rubydoc.info/gems/yard/file/docs/GettingStarted.md
 # http://www.rubydoc.info/gems/yard/file/docs/Tags.md#taglist
 
+module SPDoc
+
 # CLI tool to create new SPDoc ticket.
 # @author Igor Lesik 2017
 #
-class SPDocTicketTool
+class TicketTool
+
+class Ticket
+end
+
+def ask_category(tkt)
+    tkt
+end
+
+def ask_year(tkt)
+    tkt
+end
+
+def ask_user_name(tkt)
+    tkt
+end
+
+def ask_title(tkt)
+    tkt
+end
 
 def run
     puts "HHi"
+    ask_title ask_user_name ask_year ask_category Ticket.new
 end
 
-end
+end # class TicketTool
 
-tool = SPDocTicketTool.new
+end # module SPDoc
+
+tool = SPDoc::TicketTool.new
 tool.run
